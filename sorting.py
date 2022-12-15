8140fe56d9ebd1a5560832e35ae5ad7894b37251
Yng-pav <68937182+pavel195@users.noreply.github.com>
2022-12-15 19:25:40 +0300

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -6,15 +6,24 @@ def bubbleSort(array):
                 array[j],array[j+1] = array[j+1],array[j]
     return array
 
-
+def choice(array):
+    l = len(array)
+    for i in range(l-1):
+        m = array[i]          # идем с лева на право от 0 элемента до конца
+        id = i        # запоминаем индекс "минимального числа"
+        for j in range(i+1,l):
+            if m > array[j]:     # есть число меньше
+                m = array[j]       # запоминаем его и его индекс
+                id = j
+        if id!=i:           # меняем их местами
+            array[i],array[id] = array[id],array[i]
+    return array
 def insertion(array):
-    for i in range(1, len(array)):
-        x = array[i]
-        j = i - 1
-        while array[j] > x and j >= 0:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = x
+    l = len(array)
+    for i in range(1, l):
+            # идем от iтого элемента в обратную сторону и ищем элементы меньшие i-1того элемента
+        if array[i] < array[i-1]:    # идем пока не найдем число меньше iтого
+            array[i],array[i-1] = array[i-1],array[i] # меняем их местами
     return array
 
 
@@ -33,5 +42,8 @@ def insertion_binary(array):
         array[start] = key
     return array
 
-
-print(bubbleSort([3, 21, 10]))
+array = [3,100,50,60,33,-10,-20]
+print(choice(array))
+print(bubbleSort(array))
+print(choice(array))
+print(insertion(array))
